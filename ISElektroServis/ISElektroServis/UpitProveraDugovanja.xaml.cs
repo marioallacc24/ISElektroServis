45e39785555459860af14beb7a38180a2f8a9ed0
mario blagojevic <Maki97_2@hotmail.com>
2019-04-28 16:59:05 +0200

    Dodavanje upita za proveru dogovanja

--- ISElektroServis/ISElektroServis/UpitProveraDugovanja.xaml.cs
+++ ISElektroServis/ISElektroServis/UpitProveraDugovanja.xaml.cs
@@ -10,23 +10,21 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace ISElektroServis
 {
     /// <summary>
-    /// Interaction logic for ProveraDugovanja.xaml
+    /// Interaction logic for UpitProveraDugovanja.xaml
     /// </summary>
-    public partial class ProveraDugovanja : Window
+    public partial class UpitProveraDugovanja : UserControl
     {
-        public ProveraDugovanja()
+        public UpitProveraDugovanja()
         {
             InitializeComponent();
-            
         }
 
-       
-
-
+        
     }
 }
